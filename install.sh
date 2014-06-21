5d31b90ceb7d5dcbeff812ad19b8f67b44cbceb9
Pannous <info@pannous.net>
2014-06-22 01:44:05 +0200

    ''

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
-echo get ruby https://www.ruby-lang.org/en/installation/
-# get ruby gems
-wget http://production.cf.rubygems.org/rubygems/rubygems-2.2.2.zip
-unzip rubygems-2.2.2.zip
-cd rubygems-2.2.2
-./ruby setup.rb
-cd ..
+echo get ruby here https://www.ruby-lang.org/en/installation/
 
-gem install wordnet-defaultdb
-cd bin
-bundle install #english script
-cd ..
-bundle install #rails app
+# get ruby gems
+echo get rubygems here https://rubygems.org/pages/download
+# wget http://production.cf.rubygems.org/rubygems/rubygems-2.2.2.zip
+# unzip rubygems-2.2.2.zip
+# cd rubygems-2.2.2
+# ruby setup.rb
+# cd ..
+
+# gem install wordnet-defaultdb
+bundle install
