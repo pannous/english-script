0c2b27a3e5bf20f22d030d45eee5884bd59dc8b9
Pannous <info@pannous.net>
2014-09-22 21:52:45 +0200

    ''

--- install.sh
+++ install.sh
@@ -8,9 +8,18 @@ echo get rubygems here https://rubygems.org/pages/download
 # ruby setup.rb
 # cd ..
 
-# gem install wordnet-defaultdb
+git clone git@github.com:pannous/EnglishScript.tmbundle.git ~/Library/Application\ Support/TextMate/Bundles/EnglishScript.tmbundle/ 2>/dev/null
+
 echo INSTALLING DEPENDENCIES... This might take a minute or 10
-bundle install #--no-deployment --local
 
 git submodule init git@github.com:pickhardt/betty.git
 git submodule foreach git pull origin master
+
+# gem install wordnet-defaultdb
+bundle install #--no-deployment --local
+
+export ENGLISH_SCRIPT_HOME=$PWD
+echo export ENGLISH_SCRIPT_HOME=$PWD
+echo "export ENGLISH_SCRIPT_HOME=$PWD" >> ~/.bashrc
+
+sudo ln -s $ENGLISH_SCRIPT_HOME/bin/angle /usr/bin/angle
